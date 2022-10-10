4955b6144dd799ca67498e4e2660a44bb3801499
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2022-10-10 11:15:37 +0200

    har lagt till början

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.AnimatedStateListDrawable
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
