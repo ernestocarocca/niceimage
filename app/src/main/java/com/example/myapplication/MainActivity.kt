4d33d9ad6d78a186ba07f166599e824dbb52339d
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2022-10-10 11:31:13 +0200

    .

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,18 +5,21 @@ import android.graphics.drawable.AnimatedStateListDrawable
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.voice.VisibleActivityInfo
 import android.view.View
 import android.view.animation.Animation
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 
-
+lateinit var visa: View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        visa = findViewById(R.id.main_layout)
+        
 
 
 
